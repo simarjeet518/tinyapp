be499a6f6da2d0158191835a732b072faea1781a
Simarjeet Kaur <simarjeet518h@gmail.com>
2021-12-06 23:40:28 +0000

    ejs installed and added to server.js

--- express_server.js
+++ express_server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+
 const urlDatabase ={
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get("/",(req,res) => {
   res.send("Hello !");
