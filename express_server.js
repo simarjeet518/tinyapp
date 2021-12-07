9ce49895f8af3ff76ec6a8f3d7da1f19492fdc0b
Simarjeet Kaur <simarjeet518h@gmail.com>
2021-12-07 01:36:02 +0000

    body parser added & new form template added with post request

--- express_server.js
+++ express_server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extented: true}));
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  Math.random().toString(36).slice(7);
+}
 
 const urlDatabase ={
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,6 +28,15 @@ app.get("/hello",(req,res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req,res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) =>{
+  console.log(req.body);
+  res.send("ok");
+})
+
 app.get("/urls",(req,res) =>{
   const templateVars ={urls : urlDatabase};
   res.render("urls_index",templateVars);
@@ -32,6 +47,8 @@ app.get("/urls/:shortURL",(req,res) => {
   const templateVars = { shortURL: (req.params.shortURL).substring(1), longURL : urlDatabase[(req.params.shortURL).substring(1)]};
   res.render("urls_show", templateVars);
 });
+
+
 
 
 app.listen(PORT ,() => {
