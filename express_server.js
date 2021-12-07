9836bd2d44556186fa3cf1082dafbf34583bfe2e
Simarjeet Kaur <simarjeet518h@gmail.com>
2021-12-07 00:36:50 +0000

    header partial added

--- express_server.js
+++ express_server.js
@@ -29,7 +29,7 @@ app.get("/urls",(req,res) =>{
 
 
 app.get("/urls/:shortURL",(req,res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL : urlDatabase[(req.params.shortURL).substring(1)]};
+  const templateVars = { shortURL: (req.params.shortURL).substring(1), longURL : urlDatabase[(req.params.shortURL).substring(1)]};
   res.render("urls_show", templateVars);
 });
 
